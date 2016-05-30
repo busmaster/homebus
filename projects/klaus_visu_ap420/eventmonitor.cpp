6a94beb00671938beac660d11ad0fafe617ca31d
Klaus Gusenleitner <klaus.gusenleitner@gmail.com>
2016-05-30 10:49:48 +0200

    process termination shall terminate eventmonitor also

---
+++ projects/klaus_visu_ap420/eventmonitor.cpp
@@ -0,0 +1,111 @@
+#include <QProcess>
+#include <iostream>
+
+#include "eventmonitor.h"
+
+eventmonitor::eventmonitor(QObject *parent) : QObject(parent) {
+
+    eventmon = new QProcess;
+
+    connect(eventmon, SIGNAL(readyRead()), this, SLOT(readStdOut()));
+    connect(eventmon, SIGNAL(started()), this, SLOT(onStarted()));
+    connect(eventmon, SIGNAL(finished(int)), this, SLOT(onFinished(int)));
+
+#ifdef AP420
+    QString program = "/root/git/homebus/tools/eventmonitor/bin/eventmonitor";
+#else
+    QString program = "/home/germana/Oeffentlich/git/homebus/tools/eventmonitor/bin/eventmonitor";
+#endif
+    QStringList arguments;
+    arguments << "-c" << "/dev/hausbus0" << "-a" << "250";
+
+    evState = eEsWaitForStart;
+    eventmon->start(program, arguments);
+}
+
+void eventmonitor::readStdOut() {
+
+    int i;
+    int j;
+    bool doEmit = false;
+    QByteArray output = eventmon->readAllStandardOutput();
+    QList<QByteArray> ev_lines = output.split('\n');
+
+    for (i = 0; i < ev_lines.size(); i++) {
+        if (qstrcmp(ev_lines[i], "event address 240 device type DO31") == 0) {
+            evState = eEsWaitForDO31_Do;
+            ev.srcAddr = 240;
+            ev.type = eDevDo31;
+            continue;
+        } else if (qstrcmp(ev_lines[i], "event address 241 device type DO31") == 0) {
+            evState = eEsWaitForDO31_Do;
+            ev.srcAddr = 241;
+            ev.type = eDevDo31;
+            continue;
+        } else if (qstrcmp(ev_lines[i], "event address 36 device type SW8") == 0) {
+            evState = eEsWaitForSW8;
+            ev.srcAddr = 36;
+            ev.type = eDevSw8;
+            continue;
+        }
+        switch (evState) {
+        case eEsWaitForStart:
+            break;
+        case eEsWaitForDO31_Do:
+            for (j = 0; j < 31; j++) {
+                if (ev_lines[i].at(j) == '1') {
+                   ev.data.do31.digOut |= (1 << j);
+                } else {
+                   ev.data.do31.digOut &= ~(1 << j);
+                }
+            }
+            evState = eEsWaitForDO31_Sh;
+            break;
+        case eEsWaitForDO31_Sh:
+            evState = eEsWaitForStart;
+            doEmit = true;
+            break;
+        case eEsWaitForSW8:
+            for (j = 0; j < 8; j++) {
+                if (ev_lines[i].at(j) == '1') {
+                   ev.data.sw8.digInOut |= (1 << j);
+                } else {
+                   ev.data.sw8.digInOut &= ~(1 << j);
+                }
+            }
+            evState = eEsWaitForStart;
+            doEmit = true;
+            break;
+        default:
+            break;
+        }
+    }
+
+    if (doEmit) {
+        emit busEvent(&ev);
+    }
+}
+
+void eventmonitor::onStarted(){
+    std::cout << "eventmonitor started" << std::endl;
+}
+
+void eventmonitor::onFinished(int sig) {
+    std::cout << "eventmonitor finished: " << sig << std::endl;
+}
+
+bool eventmonitor::command(const char *cmd) {
+
+    eventmon->write(cmd);
+    eventmon->write("\n");
+
+    return true;
+}
+
+bool eventmonitor::waitForFinished(void) {
+
+    return eventmon->waitForFinished();
+}
+
+
+
