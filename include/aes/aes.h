7f6f0dd105fdd5bae668a074a3119b89fbd2fd4c
Klaus Gusenleitner <klaus.gusenleitner@gmail.com>
2016-12-14 12:29:47 +0100

    missing header

---
+++ include/aes/aes.h
@@ -0,0 +1,8 @@
+#ifndef _AES_H_
+#define _AES_H_
+
+#include <stdint.h>
+
+void AES128_CBC_decrypt_buffer(uint8_t* output, uint8_t* input, uint32_t length, const uint8_t* key, const uint8_t* iv);
+
+#endif //_AES_H_
