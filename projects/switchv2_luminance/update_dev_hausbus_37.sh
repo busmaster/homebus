deb7c36e56b441ba04c598fa3cea21e04bc81550
Klaus Gusenleitner <klaus.gusenleitner@gmail.com>
2017-01-03 19:52:56 +0100

    file mode

--- projects/switchv2_luminance/update_dev_hausbus_37.sh
+++ projects/switchv2_luminance/update_dev_hausbus_37.sh
@@ -2,6 +2,6 @@
 
 avr-objcopy -O binary switchv2_luminance.elf switchv2_luminance.bin
 ../../tools/addchecksum/bin/addchecksum switchv2_luminance.bin switchv2_luminance_cs.bin 6144
-../../tools/firmwareupdate/bin/firmwareupdate /dev/hausbus switchv2_luminance_cs.bin $1
+../../tools/firmwareupdate/bin/firmwareupdate /dev/hausbus switchv2_luminance_cs.bin 37
 rm switchv2_luminance.bin
 rm switchv2_luminance_cs.bin
