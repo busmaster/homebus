0be379d8b227db4d67bd99a84e08856942dcd98e
Klaus Gusenleitner <klaus.gusenleitner@gmail.com>
2017-07-29 11:36:47 +0200

    tools: mqtt: add PWM4 support

--- tools/mqtt/main.cpp
+++ tools/mqtt/main.cpp
@@ -76,6 +76,10 @@ typedef struct {
             uint8_t output;
             T_do31_output_type type;
         } do31;
+        struct {
+            uint8_t address;
+            uint8_t output;
+        } pwm4;
     } io;
     UT_hash_handle hh;
 } T_topic_desc;
@@ -248,6 +252,76 @@ static void do31_set_output(uint8_t address, uint8_t output, T_do31_output_type 
 }
 
 /*-----------------------------------------------------------------------------
+*  set a PWM4 pwm output using ReqSetValue telegram
+*/
+static int pwm4_ReqSetValue(uint8_t addr, uint8_t set, uint8_t *pwm) {
+
+    TBusTelegram        tx_msg;
+    TBusTelegram        *rx_msg;
+    unsigned long       start_time;
+    unsigned long       cur_time;
+    uint8_t             ret;
+    TBusDevSetValuePwm4 *sv;
+    bool                response_ok = false;
+    bool                timeout = false;
+
+    tx_msg.type = eBusDevReqSetValue;
+    tx_msg.senderAddr = my_addr;
+    tx_msg.msg.devBus.receiverAddr = addr;
+    tx_msg.msg.devBus.x.devReq.setValue.devType = eBusDevTypePwm4;
+    sv = &tx_msg.msg.devBus.x.devReq.setValue.setValue.pwm4;
+    sv->set = set;
+    memcpy(sv->pwm, pwm, sizeof(sv->pwm));
+
+    BusSend(&tx_msg);
+
+    /* response */
+    start_time = get_tick_count();
+    do {
+        cur_time = get_tick_count();
+        ret = BusCheck();
+        if (ret == BUS_MSG_OK) {
+            rx_msg = BusMsgBufGet();
+            if ((rx_msg->type == eBusDevRespSetValue)     &&
+                (rx_msg->msg.devBus.receiverAddr == my_addr) &&
+                (rx_msg->senderAddr == addr)) {
+                response_ok = true;
+            }
+        } else {
+            if ((cur_time - start_time) > BUS_RESPONSE_TIMEOUT) {
+                timeout = true;
+            }
+        }
+        usleep(10000);
+    } while (!response_ok && !timeout);
+
+    return response_ok ? 0 : -1;
+}
+
+/*-----------------------------------------------------------------------------
+*  set a PWM4 output
+*/
+static void pwm4_set_output(uint8_t address, uint8_t output, bool on) {
+
+    int        bitPos;
+    uint8_t    set;
+    uint8_t    pwm[BUS_PWM4_PWM_SIZE_SET_VALUE];
+
+    set = 0;
+    memset(pwm, 0, sizeof(pwm));
+    /* calculate the bit position (2 bits for each output) */
+    bitPos = output * 2;
+    if (on) {
+       set = 1 << bitPos;
+    } else {
+       set = 3 << bitPos;
+    }
+    if (pwm4_ReqSetValue(address, set, pwm) != 0) {
+        syslog(LOG_ERR, "PWM4 %d: can't set value %d to %d", address, output, on);
+    }
+}
+
+/*-----------------------------------------------------------------------------
 *  subscription callback for .../set
 */
 static void my_message_callback(struct mosquitto *mq, void *obj, const struct mosquitto_message *message) {
@@ -274,6 +348,10 @@ printf("subscribed: %s %s\n", message->topic, (char *)message->payload);
     case eBusDevTypeDo31:
         value8 = (uint8_t)strtoul((char *)message->payload, 0, 0);
         do31_set_output(cfg->io.do31.address, cfg->io.do31.output, cfg->io.do31.type, value8);
+        break;
+    case eBusDevTypePwm4:
+        value8 = (uint8_t)strtoul((char *)message->payload, 0, 0);
+        pwm4_set_output(cfg->io.pwm4.address, cfg->io.pwm4.output, value8 != 0);
         break;
     default:
         break;
@@ -322,7 +400,7 @@ static int ReadConfig(const char *pFile)  {
             break;
         }
         if (physical["type"] && (physical["type"].as<std::string>().compare("do31") == 0)) {
-            /* for DO31 */
+            /* DO31 */
             topic_entry->devtype = eBusDevTypeDo31;
             io_entry->phys_io = (uint8_t)topic_entry->devtype;
             if (physical["address"]) {
@@ -343,7 +421,25 @@ static int ReadConfig(const char *pFile)  {
                 io_entry->phys_io |= topic_entry->io.do31.output << 16;
                 io_entry->phys_io |= topic_entry->io.do31.type << 24;
             } else {
-                printf("missing digout at topic %s\n", node["topic"].as<std::string>().c_str());
+                printf("missing digout or shader at topic %s\n", node["topic"].as<std::string>().c_str());
+                break;
+            }
+        } else if (physical["type"] && (physical["type"].as<std::string>().compare("pwm4") == 0)) {
+            /* PWM4 */
+            topic_entry->devtype = eBusDevTypePwm4;
+            io_entry->phys_io = (uint8_t)topic_entry->devtype;
+            if (physical["address"]) {
+                topic_entry->io.pwm4.address = (uint8_t)strtoul(physical["address"].as<std::string>().c_str(), 0, 0);
+                io_entry->phys_io |= (topic_entry->io.pwm4.address << 8);
+            } else {
+               printf("missing address at topic %s\n", node["topic"].as<std::string>().c_str());
+               break;
+            }
+            if (physical["pwmout"]) {
+                topic_entry->io.pwm4.output = (uint8_t)strtoul(physical["pwmout"].as<std::string>().c_str(), 0, 0);
+                io_entry->phys_io |= topic_entry->io.pwm4.output << 16;
+            } else {
+                printf("missing pwmout at topic %s\n", node["topic"].as<std::string>().c_str());
                 break;
             }
         } else {
@@ -448,6 +544,35 @@ printf("publish %s %s\n", topic, payload);
     memcpy(dev_entry->io.do31.shader, av->shader, sizeof(dev_entry->io.do31.shader));
 }
 
+static void publish_pwm4(
+    T_dev_desc             *dev_entry,
+    TBusDevActualValuePwm4 *av,
+    bool                   publish_unconditional
+    ) {
+    int                    i;
+    int                    bitPos;
+    uint8_t                actval8;
+    uint32_t               phys_io;
+    T_io_desc              *io_entry;
+    char                   topic[MAX_LEN_TOPIC];
+
+    for (i = 0; i < 4; i++) {
+        bitPos = i;
+        if (publish_unconditional ||
+            ((dev_entry->io.pwm4.state & (1 << bitPos)) != (av->state & (1 << bitPos)))) {
+            phys_io = dev_entry->phys_dev | (i << 16);
+            HASH_FIND_INT(io_desc, &phys_io, io_entry);
+            if (io_entry) {
+                actval8 = (av->state & (1 << bitPos)) != 0;
+                snprintf(topic, sizeof(topic), "%s/actual", io_entry->topic);
+printf("publish %s %d\n", topic, actval8);
+                mosquitto_publish(mosq, 0, topic, 1, actval8 ? "1" : "0", 1, true);
+            }
+        }
+    }
+    dev_entry->io.pwm4.state = av->state;
+}
+
 static void serve_bus(void) {
     uint8_t                     busRet;
     TBusTelegram                *pRxBusMsg;
@@ -479,6 +604,9 @@ static void serve_bus(void) {
     switch (dev_type) {
     case eBusDevTypeDo31:
         publish_do31(dev_entry, &ave->actualValue.do31, false);
+        break;
+    case eBusDevTypePwm4:
+        publish_pwm4(dev_entry, &ave->actualValue.pwm4, false);
         break;
     default:
         break;
@@ -549,6 +677,8 @@ printf("publish init state: DO31 at %d\n", address);
                 publish_do31(dev_entry, &av->actualValue.do31, true);
                 break;
             case eBusDevTypePwm4:
+printf("publish init state: PWM4 at %d\n", address);
+                publish_pwm4(dev_entry, &av->actualValue.pwm4, true);
                 break;
             default:
                 break;
@@ -671,7 +801,19 @@ int main(int argc, char *argv[]) {
     maxFd = busFd;
 
     HASH_ITER(hh, topic_desc, topic_entry, topic_tmp) {
-        printf("topic %s %d %d %d\n", topic_entry->topic, topic_entry->devtype,  topic_entry->io.do31.address, topic_entry->io.do31.output);
+        printf("topic %s: type " , topic_entry->topic);
+        switch (topic_entry->devtype) {
+        case eBusDevTypeDo31:
+            printf("DO31, address %d, output %d, type %d", topic_entry->io.do31.address, topic_entry->io.do31.output, topic_entry->io.do31.type);
+            break;
+        case eBusDevTypePwm4:
+            printf("PWM4, address %d, output %d", topic_entry->io.pwm4.address, topic_entry->io.pwm4.output);
+            break;
+        default:
+            printf("(unknown)");
+            break;
+        }
+        printf("\n");
     }
     HASH_ITER(hh, dev_desc, dev_entry, dev_tmp) {
         printf("dev %04x\n", dev_entry->phys_dev);
@@ -686,7 +828,7 @@ int main(int argc, char *argv[]) {
     mosq_connected = true;
     mosqFd = mosquitto_socket(mosq);
     init_state();
-    /* subscribe to all configured topic extended by 'set' */
+    /* subscribe to all configured topics extended by 'set' */
     HASH_ITER(hh, topic_desc, topic_entry, topic_tmp) {
         snprintf(topic, sizeof(topic), "%s/set", topic_entry->topic);
         mosquitto_subscribe(mosq, 0, topic, 1);
@@ -700,7 +842,7 @@ int main(int argc, char *argv[]) {
             }
             mosqFd = mosquitto_socket(mosq);
             init_state();
-            /* subscribe to all configured topic extended by 'set' */
+            /* subscribe to all configured topics extended by 'set' */
             HASH_ITER(hh, topic_desc, topic_entry, topic_tmp) {
                 snprintf(topic, sizeof(topic), "%s/set", topic_entry->topic);
                 mosquitto_subscribe(mosq, 0, topic, 1);
