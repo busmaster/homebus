14f943a7b684940cb6af319b86c08ddb18977ac6
Klaus Gusenleitner <klaus.gusenleitner@gmail.com>
2026-01-18 17:43:54 +0100

    DO31 bootloader: Add bootloader for devices using 1MHz MCU clock

---
+++ projects/bootloader/do31_bootloader_1MHz/flashasm.h
@@ -0,0 +1,55 @@
+/*
+ * flashasm.h
+ * 
+ * Copyright 2013 Klaus Gusenleitner <klaus.gusenleitner@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+#ifndef _FLASHASM_H
+#define _FLASHASM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+/*-----------------------------------------------------------------------------
+*  Macros
+*/                     
+
+/*-----------------------------------------------------------------------------
+*  typedefs
+*/
+
+/*-----------------------------------------------------------------------------
+*  Variables
+*/                                
+
+/*-----------------------------------------------------------------------------
+*  Functions
+*/
+void FlashFillPagePuffer(uint16_t offset, uint16_t *pBuf, uint8_t numWords);
+void FlashProgramPagePuffer(uint16_t pageWordAddr);
+void FlashPageErase(uint16_t pageWordAddr);
+
+#ifdef __cplusplus
+}
+#endif 
+#endif
